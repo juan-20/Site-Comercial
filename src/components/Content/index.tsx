4fdb64c2a582a918e3e2e7bf54de78eca26381d9
Juan Andrade <49418565+juan-20@users.noreply.github.com>
2021-02-10 17:38:44 -0300

    Estilização :nail_care:

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Product from './Product';
+import { Container } from './styles';
+
+
+const Content: React.FC = () => {
+  return (
+    <Container>
+        <Product />
+    </Container>
+
+  );
+};
+
+export default Content;
